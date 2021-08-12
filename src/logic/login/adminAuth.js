877721b969db132c17bdbb2628b3268a62b5f945
Luca Goettle <lgoettle@stud.hs-heilbronn.de>
2021-08-12 16:24:12 +0200

    fix: reject error

--- src/logic/login/adminAuth.js
+++ src/logic/login/adminAuth.js
@@ -4,10 +4,12 @@ import HttpError from '../../error/HttpError';
 export default function adminAuth(data) {
   return new Promise((resolve, reject) => {
     if (data == null) throw HttpError.Unauthorized('authorization is empty');
-    Admin.findById(data._id).then((user) => {
-      if (user == null) reject(HttpError.Unauthorized('User not found'));
-      if (user && user.confirmed !== true) reject(HttpError.Unauthorized('User awaits confirmation'));
-      resolve(user);
-    });
+    Admin.findById(data._id)
+      .then((user) => {
+        if (user == null) reject(HttpError.Unauthorized('User not found'));
+        if (user && user.confirmed !== true) reject(HttpError.Unauthorized('User awaits confirmation'));
+        resolve(user);
+      })
+      .catch((err) => { reject(err); });
   });
 }
