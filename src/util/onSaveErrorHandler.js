15efb7c45f0184facd2a54d57826c8baa92743e4
Luca Goettle <lgoettle@stud.hs-heilbronn.de>
2021-08-16 18:03:46 +0200

    fix: access message as prop

--- src/util/onSaveErrorHandler.js
+++ src/util/onSaveErrorHandler.js
@@ -4,5 +4,5 @@ export default function onSaveErrorHandler(err) {
   if (err && err.isHttpError) {
     throw err;
   }
-  throw HttpError.BadRequest(err.message());
+  throw HttpError.BadRequest(err.message);
 }
